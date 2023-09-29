43809050f300bb04b171e2b99ba2084c7c2be0ff
aconantguy <aidan@conantguy.org>
2023-09-29 00:10:11 -0500

    Completed Lab

--- EmployeeApp.java
+++ EmployeeApp.java
@@ -3,18 +3,72 @@ import java.util.Scanner;
 class Employee
 {
 	/* Task 3B: Add instance variables */
-  
-   
-	/* Task 3C: Add three constructors */
-   
-   
-	/* Task 3D: Add set (mutator) and get (accessor) meethods
+	
+	private String firstName, lastName;
+	private int employeeID;  
+   	private double salary;
 
+	/* Task 3C: Add three constructors */
+	
+	public Employee(){
+	}
+
+	public Employee(String last, String first){
+		firstName = first;
+		lastName = last;
+	}
+
+	public Employee(String last, String first, int id, double wage){	
+		firstName = first;
+		lastName = last;
+		employeeID = id;
+		salary = wage;
+
+	}
    
+	// Task 3D: Add set (mutator) and get (accessor) meethods
+	public void setFirstName(String first){
+		firstName = first;
+	}   
+	
+	public void setLastName(String last){
+		lastName = last;
+	}
+
+	public void setEmployeeId(int id){
+		employeeID = id;
+	}
+
+	public void setSalary(double wage){
+		if (wage < 0){
+			wage = 0;
+		}
+		salary = wage;
+	}
+
+	public String getFirstName(){
+		return firstName;
+	}
+
+	public String getLastName(){
+		return lastName;
+	}
+
+	public int getEmployeeId(){
+		return employeeID;
+	}
+
+	public double getSalary(){
+		return salary;
+	}
 	/* Task 3F: Add toString method */
-   
-   
+   	public String toString(){
+		return ("Name: "+firstName+" "+lastName+"\nEmployee ID: "+employeeID+"\nSalary: $"+salary+"\n");  
+  	} 
 	/* Task 3G: Add equals method */
+	public boolean equals( String name ){
+		return (lastName.equals(name));
+	}
    
 }
 
@@ -53,16 +107,33 @@ public class EmployeeApp
 			switch( choice )
 			{
 				case 'A':
-					if ( empCount < MAX_EMPLOYEES-1 )
+					if ( empCount < MAX_EMPLOYEES )
 					{
 						// Create object for new Employee
 						employees[empCount] = new Employee();
                 
 						// Task 3E: Prompt for user information and set the object 
 						// parameters via the mutator methods
+						System.out.print("Enter First name: ");
+						String first = keyboard.nextLine();
+						employees[empCount].setFirstName(first);
+						
+						System.out.print("Enter Last name: ");
+						String last = keyboard.nextLine();
+						employees[empCount].setLastName(last);
+						
+						System.out.print("Enter Employee ID: ");
+						int id = keyboard.nextInt();
+						employees[empCount].setEmployeeId(id);
 
-   
+						System.out.print("Enter Salary: ");
+						double wage = keyboard.nextDouble();
+						employees[empCount].setSalary(wage);
+						
 						empCount++;
+					}
+					else{ // Notifies user if an MAX_EMPLOYEES has been reached
+						System.out.println("Database full");
 					}
 					keyboard.nextLine();
 				break;
@@ -70,6 +141,7 @@ public class EmployeeApp
 				case 'E':
 					System.out.printf( "Enter Last Name of Employee to Edit: " );
 					inputString = keyboard.nextLine();
+					boolean match = false;
 					for ( int lp=0; lp<MAX_EMPLOYEES; lp++ )
 					{
 						// Verify that the employee entry has been allocated before Editing
@@ -85,18 +157,27 @@ public class EmployeeApp
 								inputDouble = keyboard.nextDouble();
 								employees[lp].setSalary( inputDouble );
    
+								match = true;
 								keyboard.nextLine();
 							}
 						}
 					}
+				if(match == false){ // Notifies user if no match has been found
+					System.out.println("No match found.");
+				}
 				break;
-              
+        
 				case 'L':
 					for ( int lp=0; lp<MAX_EMPLOYEES; lp++ )
 					{
 						System.out.println( employees[lp] );
 					}
 				break;
+				
+				default: // Notifies user if an invalid selection has been made
+					if( choice != 'Q') {
+						System.out.println("Invalid Input");
+					}
 			}
 			keyboard.reset();
 		} while( choice != 'Q' );
